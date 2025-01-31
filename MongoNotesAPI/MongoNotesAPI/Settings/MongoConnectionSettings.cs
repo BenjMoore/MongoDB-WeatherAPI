d0b0c19f34d850de9edd4034c53692f7a9d69727
Perl <72554458+BenjMoore@users.noreply.github.com>
2025-01-31 15:11:49 +1000

    push

--- MongoNotesAPI/MongoNotesAPI/Settings/MongoConnectionSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MongoNotesAPI.Settings
-{
-    public class MongoConnectionSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
